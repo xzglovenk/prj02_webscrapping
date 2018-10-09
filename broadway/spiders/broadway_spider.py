ced1256e6d278b0410e29f2ff176660ff44e629a
Zhenggang Xu <5289500+xzglovenk@users.noreply.github.com>
2018-10-09 14:48:12 -0400

    10092018_cleanup

--- broadway/spiders/broadway_spider.py
+++ broadway/spiders/broadway_spider.py
@@ -9,35 +9,33 @@ class BroadwaySpider(Spider):
 
 	def parse(self, response):
 
-#		num_reviews = response.xpath('//div[@class="rs rating"]/a[@class="more"]/span/text()').extract_first()
 
 		num_reviews = response.xpath('//div[@class="listContainer responsive"]/p/b[3]/text()').extract_first()
 		num_reviews = int(num_reviews.replace(',', ''))
 		last_page = num_reviews//10
 		last_page2 = (last_page + 1)*10
-		# print('#'*50)
-		# print(num_reviews, last_page, last_page2)
-		# print('#'*50)
+
 
 		pagelist_urls = ["https://www.tripadvisor.com/Attraction_Review-g60763-d1383285-Reviews-Jersey_Boys-New_York_City_New_York.html"] + ["https://www.tripadvisor.com/Attraction_Review-g60763-d1383285-Reviews-or{}-Jersey_Boys-New_York_City_New_York.html".format(i) for i in range(10, last_page2, 10)]
 
+		# Loop through different review pages
 		for url in pagelist_urls:
-			#print(url)
+		
 			yield Request(url = url, callback = self.parse_review_page)
 
 	def parse_review_page(self, response):
+
+		# For each review page, collect all the review links into a list
 		reviews_urls = response.xpath('//div[@class="prw_rup prw_reviews_basic_review_responsive"]//div[@class="wrap"]//a/@href').extract()
 
-		# print('#'*50)
-		# print(len(reviews_urls))
-		# print('#'*50)
-		
-
+		# Loop through all the links
 		for url in reviews_urls[::2]:
-			
+		# Get into every link in the next level	
 			yield Request(url = "https://www.tripadvisor.com"+url, callback = self.parse_detail_page)
 
 	def parse_detail_page(self, response):
+
+		# For each review, grab all the desired information
 		showname = response.xpath('//div[@id="listing_main_sur"]//div[@class="surContent"]/a/text()').extract_first().strip()
 		title = response.xpath('//div[@class="wrap"]//span[@class="noQuotes"]/text()').extract_first()
 		text_temp = response.xpath('//div[@class="listContainer responsive"]')[0]
@@ -49,17 +47,6 @@ class BroadwaySpider(Spider):
 		contribution, helpful = text_temp.xpath('//div[@class="memberBadgingNoText"]/span[@class="badgetext"]/text()').extract()[:2]
 
 
-
-		# print('#'*50)
-		# print(title)
-		# print(full_text)
-		# print(rating)
-		# print(date)
-		# print(user)
-		# print(contribution)
-		# print(helpful)
-		# print('#'*50)
-
 		item = BroadwayItem()
 		item['showname'] = showname
 		item['title'] = title
@@ -69,19 +56,7 @@ class BroadwaySpider(Spider):
 		item['user'] = user
 		item['contribution'] = contribution
 		item['helpful'] = helpful
-		# item['user_answers'] = user_answers
-		# item['user_questions'] = user_questions
-		# item['user_join'] = user_join
+	
 		yield item
 
 	
-		# vote = response.xpath('//div[@class="vote"]/span[@itemprop="upvoteCount"]/text()').extract_first()
-		# user = response.xpath('//div[@class="question"]//div[@class="post-signature owner grid--cell fl0"]//div[@class="user-info "]//a/text()').extract_first()
-		# answer = response.xpath('//div[@id="answers"]//h2/@data-answercount').extract_first()
-		# viewed = response.xpath('//div[@class="module question-stats"]/table//tr[2]/td[2]/p/b/text()').extract_first()
-		# date = response.xpath('//div[@class="module question-stats"]/table//tr[1]/td[2]/p/@title').extract_first()
-		# viewed = re.search('\d+', viewed.replace(',', '')).group()
-		# date = re.search('[0-9]{4}-[0-9]{2}-[0-9]{2}', date).group()
-		# user_url = response.xpath('//div[@class="question"]//div[@class="post-signature owner grid--cell fl0"]//div[@class="user-info "]//a/@href').extract_first()
-		# user_urlfull = "https://stackoverflow.com/"+user_url
-
